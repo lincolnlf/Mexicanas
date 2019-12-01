b8ce58fa72ae599806bb258b42136618d38cad22
ribeiroarthur <56894765+ribeiroarthur@users.noreply.github.com>
2019-12-01 00:09:43 -0300

    Usuário comprando produto

--- Loja.cs
+++ Loja.cs
@@ -6,51 +6,30 @@ using System.Text;
 
 public class Loja{
    
-    string str;
-    string str2;
+    List<Produto> produtos = new List<Produto>();
+    
+    public void comprarumproduto(int id){
+        
+    }
 
-    List<Produto> produto = new List<Produto>();
-    
-    
     public void ComprarProduto(){
 
-      FileStream meuArq = new FileStream("produtoparavenda.txt", FileMode.Open, FileAccess.Read);
-
-      StreamReader sr = new StreamReader(meuArq, Encoding.UTF8);
-
-
-      FileStream meuArq2 = new FileStream("QuantidadeAtual.txt", FileMode.Open, FileAccess.Read);
-
-      StreamReader sr2 = new StreamReader(meuArq2, Encoding.UTF8);
+      string[] linhas = File.ReadAllLines("produtoparavenda.txt");
       
+      foreach(string linha in linhas){
+        string[] dados = linha.Split(";");
+        Produto p = new Produto(dados[0], float.Parse(dados[1]), Int16.Parse(dados[2]),Int16.Parse(dados[3]));
+        produtos.Add(p);
+        
+      }
+      
+    }
+
+    public void MostrarNaLista(){     
+    }
+
     
-      while(!sr.EndOfStream){
-        /*
-        str = sr.ReadLine();
-        str2 = sr2.ReadLine();
-      
 
-        string nomeProduto;
-        int quantidade;
-        float valor;
-
-        nomeProduto = String.Join("", System.Text.RegularExpressions.Regex.Split(str, @"[\d| -]"));
-
-        valor = float.Parse(String.Join("", System.Text.RegularExpressions.Regex.Split(str, @"[^\d]")));
-
-        quantidade = int.Parse(str2);
-        produto.Add(new Produto(nomeProduto,valor,quantidade));
-        */
-        //Console.WriteLine(nomeProduto+" - "+ valor + " - "+quantidade);
-
-      }
-    }
-    public void MostrarProduto(){
-
-      foreach(Produto p in produto){
-        Console.WriteLine(p);
-      }
-    }
     }
 
 
