e6bb35169ad402392fc4a5b0b850c4ac6f23c1f5
ribeiroarthur <56894765+ribeiroarthur@users.noreply.github.com>
2019-12-02 22:43:17 -0300

    Pessoa fisica

--- Usuario.cs
+++ Usuario.cs
@@ -7,11 +7,11 @@ public class Usuario{
 
   List<Usuario> usuarios = new List<Usuario>();
 
-  private string email;
-  private string nome;
-  private string cep;
 
-  private string senha;
+  protected  string email;
+  protected string nome;
+  protected string cep;
+  protected string senha;
   
   public string getEmail(){
     return email;
@@ -69,80 +69,6 @@ public class Usuario{
   }
  
 
-  public  void CadastroUsuario(string n, string e ,string  cp, string  s ){
-    int id = qtdLinhas();
-    if(VerificarCadastro(email)){
- 
-    StreamWriter streamWriter = File.AppendText("CadastroUsuario.txt");
-    streamWriter.WriteLine(n +";" + e  + ";" + id + ';' + cp +';'+s);
-    streamWriter.Close();
-    }
-    else{
-      Console.WriteLine("Esse email já foi cadastrado");
-    }
-        
-    
-  } 
-
-  private void AtualizarLista(){
-    
-    string[] linhas = File.ReadAllLines("CadastroUsuario.txt");
-    usuarios.Clear();
-   
-    
-    foreach(string linha in linhas){
- 
-      string[] dados = linha.Split(";");
- 
-      Usuario usuario = new Usuario(dados[0],dados[1],dados[2], dados[3]);
-      usuarios.Add(usuario);
-      
-    }
-  } 
-
-  public static int qtdLinhas(){
-    FileStream  leiturarqvoluntario= new FileStream("CadastroUsuario.txt",FileMode.Open,FileAccess.Read);
-    StreamReader lerinfobasic =new StreamReader(leiturarqvoluntario,Encoding.UTF8);
-    int id = 1;
-    while(!lerinfobasic.EndOfStream){
-      lerinfobasic.ReadLine();
-      id++;
-    } 
-    lerinfobasic.Close();
-    leiturarqvoluntario.Close();
-    return id;
-  }
-
-  private bool  VerificarCadastro(string email){
-    
-    AtualizarLista();
-    
-    foreach(Usuario  u in usuarios){
-      if ( email == u.getEmail() ){
-
-        return false;
-      }
-
-    } 
-
-    return true;
-
-  }
-
-  public bool Login( string email, string senha){
-     AtualizarLista();
-    
-    foreach(Usuario  u in usuarios){
-      if ( email == u.getEmail() && senha == u.getSenha() ){
-        
-           return true;
-      }
-    } 
-    return false;
-
- 
-
-  }
   
 
 
