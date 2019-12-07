c2f3e5c5933b2105c5d5341fab48014812c22874
lincolnlf <50423652+lincolnlf@users.noreply.github.com>
2019-12-06 22:51:00 -0300

    Add files via upload

--- Fisico.cs
+++ Fisico.cs
@@ -2,14 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-
+// Classe físico herda usuário
 public class  Fisico : Usuario{
 
   List<Usuario> usuarios = new List<Usuario>();
+
+  
   
   private string cpf;
 
-
+// Construtores
   public Fisico(){
     cpf = "CPF";
    
@@ -28,58 +30,10 @@ public class  Fisico : Usuario{
 
    public void setCPF(string c){
       cpf = c;
+
    }
-
-
- public  void CadastroUsuario(string n, string e ,string  cp, string  s, string cf ){
-    int id = qtdLinhas();
-    if(VerificarCadastro(email)){
- 
-    StreamWriter streamWriter = File.AppendText("PessoaFisica.txt");
-    streamWriter.WriteLine(n +";" + e  + ";" + id + ';' + cp +';'+s +';'+cf );
-    streamWriter.Close();
-    }
-    else{
-      Console.WriteLine("Esse email já foi cadastrado");
-    }
-        
-    
-  } 
-
-  private void AtualizarLista(){
-    
-    string[] linhas = File.ReadAllLines
-    
-    ("PessoaFisica.txt");
-      
-
-    usuarios.Clear();
-   
-    
-    foreach(string linha in linhas){
- 
-      string[] dados = linha.Split(";");
- 
-      Fisico usuario = new Fisico(dados[0],dados[1],dados[2], dados[3] , dados[4]);
-      usuarios.Add(usuario);
-      
-    }
-  } 
-
-  public static int qtdLinhas(){
-    FileStream  leiturarqvoluntario= new FileStream("PessoaFisica.txt",FileMode.Open,FileAccess.Read);
-    StreamReader lerinfobasic =new StreamReader(leiturarqvoluntario,Encoding.UTF8);
-    int id = 1;
-    while(!lerinfobasic.EndOfStream){
-      lerinfobasic.ReadLine();
-      id++;
-    } 
-    lerinfobasic.Close();
-    leiturarqvoluntario.Close();
-    return id;
-  }
-
-  public bool  VerificarCadastro(string email){
+// Verificação de cadastro de usuário
+   public bool  VerificarCadastro(string email){
     
     AtualizarLista();
     
@@ -94,11 +48,59 @@ public class  Fisico : Usuario{
     return true;
 
   }
+  // Leitura de arquivo voluntário
+  public static int qtdLinhas(){
+    FileStream  leiturarqvoluntario= new FileStream("PessoaFisica.txt",FileMode.Open,FileAccess.Read);
+    StreamReader lerinfobasic =new StreamReader(leiturarqvoluntario,Encoding.UTF8);
+    int id = 1;
+    while(!lerinfobasic.EndOfStream){
+      lerinfobasic.ReadLine();
+      id++;
+    } 
+    lerinfobasic.Close();
+    leiturarqvoluntario.Close();
+    return id;
+  }
 
-  public bool Login( string email, string senha){
-
+// cadastro de usuário
+ public  void CadastroUsuario(string n, string e ,string  cp, string  s, string cf ){
+    int id = qtdLinhas();
+    if(VerificarCadastro(email)){
+ 
+    StreamWriter streamWriter = File.AppendText("PessoaFisica.txt");
+    streamWriter.WriteLine(n +";" + e  + ";" + id + ';' + cp +';'+s +';'+cf );
+    streamWriter.Close();
+    }
+    else{
+      Console.WriteLine("Esse email já foi cadastrado");
+    }
+        
+    
+  } 
+// Atualização de lista
+  private void AtualizarLista(){
+    
+    string[] linhas = File.ReadAllLines
+    
+    ("PessoaFisica.txt");
       
 
+    usuarios.Clear();
+   
+    
+    foreach(string linha in linhas){
+ 
+      string[] dados = linha.Split(";");
+     
+ 
+      Fisico usuario = new Fisico(dados[0],dados[1],dados[2], dados[3] , dados[4]);
+      usuarios.Add(usuario);
+      
+    }
+  } 
+// Sessão de login
+  public bool Login( string email, string senha){
+      
      AtualizarLista();
     
     foreach(Usuario  u in usuarios){
@@ -108,10 +110,6 @@ public class  Fisico : Usuario{
       }
     } 
     return false;
-
- 
-
   }
-  
 
 }
